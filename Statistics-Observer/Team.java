10e12f44fd4f633852dbcb23a596db7a69671b84
lodianabeqiri <lodianabeqiri@yahoo.com>
2018-01-26 20:43:56 +0100

    Add files via upload

---
+++ Statistics-Observer/Team.java
@@ -0,0 +1,74 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+
+	private List<Player> team = new ArrayList<>();
+	private String name;
+	private TeamStatistic stat;
+	
+
+	
+	private int matchWon = 0; 
+	private List<Tournament> tournamentsWon = new ArrayList<>();
+	
+	public Team(String name){
+		this.name = name;
+	}
+	
+	//show statisctics
+
+	public void showStatistic(TeamStatistic statictic){
+			
+			statictic.showStat(this);
+	}
+	
+	//get methods
+	
+	public TeamStatistic getStatistics() {
+		return this.stat;
+	}
+
+	public void setStat(TeamStatistic stat) {
+		this.stat = stat;
+	}
+
+	protected String getName(){
+		return this.name;
+	}
+	
+	public List<Player> getPlayers(){
+		return this.team;
+	}
+	
+	public List<Tournament> getTournamentsWon(){
+		return this.tournamentsWon;
+	}
+	
+	//----
+	
+	public void addPlayer(Player player){
+		this.team.add(player);
+	}
+	
+	private void removePlayer(Player player){
+		this.team.remove(player);
+	}
+	
+	public void addTournamentWon(Tournament t){
+		this.tournamentsWon.add(t);
+	}
+	
+	public void addMatchWon() {
+		this.matchWon++;
+		for(Player p : team) {
+			p.getPcard().addMatchWon();
+		}
+	}
+	
+	public int getMatchesWon() {
+		return this.matchWon;
+	}
+	
+}
